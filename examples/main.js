72213ca875f2d840ec681daf8a375ce3dcdbba2e
smileHuang_hhm <1581302023@qq.com>
2024-11-04 09:44:41 +0800

    Initial commit

---
+++ examples/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// 导入组件库
+import PdfScreenshot from '../packages/index';
+
+console.log('PdfScreenshot', PdfScreenshot)
+// 注册组件库
+Vue.use(PdfScreenshot)
+
+Vue.config.productionTip = false
+
+new Vue({
+    render: h => h(App)
+}).$mount('#app')
---
+++ packages/pdf-screenshot/src/pdf-screenshot.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="pdf-screenshot">
+    hhm
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PdfScreenshot',
+  mounted() {
+
+  },
+  methods: {
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
